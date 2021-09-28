0dbe49f5146b9fb18f2a4b406df9bd4c917b22e4
Dariia Kachalova <danniednepr@gmail.com>
2021-09-28 21:24:02 +0200

    added option to activate order/booking by clicking on same component on the Home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
-import {settings, select, classNames} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
 import Home from './components/Home.js';
 
-const app = {
-  initPages: function(){
+export const app = {
+  initPages: function () {
     const thisApp = this;
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
-    
+
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     const idFromHash = window.location.hash.replace('#/', '');
@@ -16,18 +16,17 @@ const app = {
 
     let pageMatchingHash = thisApp.pages[0].id;
 
-    for (let page of thisApp.pages){
-      if(page.id == idFromHash){
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
         pageMatchingHash = page.id;
         break;
       }
-
     }
 
     thisApp.activatePage(pageMatchingHash);
 
-    for (let link of thisApp.navLinks){
-      link.addEventListener('click', function(event){
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
         const clickedElement = this;
         event.preventDefault();
 
@@ -38,39 +37,40 @@ const app = {
 
         //change URL hash
         window.location.hash = '#/' + id;
-
       });
     }
   },
-  activatePage: function(pageId){
+  activatePage: function (pageId) {
     const thisApp = this;
     //add class 'active' to matching pages, remove from non-matching
-    for(let page of thisApp.pages){
+    for (let page of thisApp.pages) {
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
-    
+
     //add class 'active' to matching links, remove from non-matching
-    for(let link of thisApp.navLinks){
+    for (let link of thisApp.navLinks) {
       link.classList.toggle(
-        classNames.nav.active, 
+        classNames.nav.active,
         link.getAttribute('href') == '#' + pageId
       );
     }
   },
-  initMenu: function(){
+  initMenu: function () {
     const thisApp = this;
     //console.log('thisApp.data:', thisApp.data);
-    for(let productData in thisApp.data.products){
+    for (let productData in thisApp.data.products) {
       // new Product(productData, thisApp.data.products[productData]);
-      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+      new Product(
+        thisApp.data.products[productData].id,
+        thisApp.data.products[productData]
+      );
     }
   },
 
- 
-  initCart: function() {
+  initCart: function () {
     const thisApp = this;
     const cartElem = document.querySelector(select.containerOf.cart);
-    thisApp.cart = new Cart (cartElem);
+    thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
@@ -79,15 +79,15 @@ const app = {
     });
   },
 
-  initData: function(){
+  initData: function () {
     const thisApp = this;
     thisApp.data = {};
     const url = settings.db.url + '/' + settings.db.products;
     fetch(url)
-      .then(function(rawResponse){
+      .then(function (rawResponse) {
         return rawResponse.json();
       })
-      .then(function(parsedResponse){
+      .then(function (parsedResponse) {
         // console.log('parsedResponse:', parsedResponse);
         /* save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
@@ -97,22 +97,20 @@ const app = {
     //console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
-  initBooking: function() {
+  initBooking: function () {
     const thisApp = this;
 
     const bookingWidget = document.querySelector(select.containerOf.booking);
     thisApp.booking = new Booking(bookingWidget);
-
   },
-  initHome: function(){
+  initHome: function () {
     const thisApp = this;
 
     const homeWidget = document.querySelector(select.containerOf.home);
     thisApp.home = new Home(homeWidget);
-
   },
 
-  init: function(){
+  init: function () {
     const thisApp = this;
     thisApp.initPages();
     thisApp.initData();
@@ -120,7 +118,7 @@ const app = {
     thisApp.initCart();
     thisApp.initBooking();
     thisApp.initHome();
-  },  
+  },
 };
 
 app.init();
--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,9 +1,10 @@
 import { select, templates } from '../settings.js';
-
+import {app} from '../app.js';
 class Home {
   constructor(element) {
     const thisHome = this;
     thisHome.render(element);
+    thisHome.goToPage();
   }
 
   render(element) {
@@ -17,7 +18,15 @@ class Home {
     thisHome.dom.order = thisHome.dom.wrapper.querySelector(select.home.order);
     thisHome.dom.book = thisHome.dom.wrapper.querySelector(select.home.book);
   }
-
+  goToPage(){
+    const thisHome = this;
+    thisHome.dom.order.addEventListener('click', function(){
+      app.activatePage('order');
+    });
+    thisHome.dom.book.addEventListener('click', function(){
+      app.activatePage('booking');
+    });
+  }
 }
 
 
