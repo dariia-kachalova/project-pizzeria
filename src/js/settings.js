23fe81ffe94156b03472ad9537aaaea66fae5790
Dariia Kachalova <danniednepr@gmail.com>
2021-09-29 00:02:23 +0200

    heroku: beginning

--- src/js/settings.js
+++ src/js/settings.js
@@ -137,7 +137,7 @@ export const settings = {
     links: '.main-nav a',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
